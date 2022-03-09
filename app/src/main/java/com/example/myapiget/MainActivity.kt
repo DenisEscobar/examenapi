3687043aed4441e41136f3efa1f703c6230baa3c
dam2 <a>
2022-03-09 19:43:36 +0100

    a

--- app/src/main/java/com/example/myapiget/MainActivity.kt
+++ app/src/main/java/com/example/myapiget/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.myapiget
 
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
-import android.widget.SearchView
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+//import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,9 +12,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cursokotlin.retrofitkotlinexample.APIService
 import com.cursokotlin.retrofitkotlinexample.DogsAdapter
 import com.cursokotlin.retrofitkotlinexample.DogsResponse
+import com.squareup.picasso.Picasso
 import org.jetbrains.anko.alert
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -21,25 +27,33 @@ class MainActivity : AppCompatActivity(), androidx.appcompat.widget.SearchView.O
     lateinit var imagesPuppies:List<String>
     lateinit var dogsAdapter: DogsAdapter
 
-    lateinit var searchBreed: SearchView
+    lateinit var searchBreed: androidx.appcompat.widget.SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         searchBreed = findViewById(R.id.searchBreed)
-//        searchBreed.setOnQueryTextListener(this)
+        searchBreed.setOnQueryTextListener(this)
+
+        searchByName("Akita")
     }
 
     private fun initCharacter(puppies: DogsResponse) {
         if(puppies.status == "success"){
             imagesPuppies = puppies.images
+            alert(""+imagesPuppies)
+            val rvDog = findViewById<ImageView>(R.id.imageView)
+            Picasso.get().load(""+imagesPuppies).into(rvDog)
         }
-        val rvDogs = findViewById<RecyclerView>(R.id.rvDogs)
-        dogsAdapter = DogsAdapter(imagesPuppies)
-        rvDogs.setHasFixedSize(true)
-        rvDogs.layoutManager = LinearLayoutManager(this)
-        rvDogs.adapter = dogsAdapter
+
+        //Picasso.with(this).load(imagesPuppies).into(rvDogs)
+
+//        val rvDogs = findViewById<RecyclerView>(R.id.rvDogs)
+//        dogsAdapter = DogsAdapter(imagesPuppies)
+//        rvDogs.setHasFixedSize(true)
+//        rvDogs.layoutManager = LinearLayoutManager(this)
+//        rvDogs.adapter = dogsAdapter
     }
 
     private fun getRetrofit(): Retrofit {
@@ -50,18 +64,36 @@ class MainActivity : AppCompatActivity(), androidx.appcompat.widget.SearchView.O
     }
 
     private fun searchByName(query: String) {
-        doAsync {
-            val call = getRetrofit().create(APIService::class.java).getCharacterByName("$query/images").execute()
-            val puppies = call.body() as DogsResponse
-            uiThread {
-                if(puppies.status == "success") {
-                    initCharacter(puppies)
-                }else{
-                    showErrorDialog()
+        val api = getRetrofit().create(APIService::class.java)
+        api.getCharacterByName("$query/images").enqueue(object : Callback<DogsResponse> {
+            override fun onResponse(call: Call<DogsResponse>, response: Response<DogsResponse>) {
+                val heroList: List<DogsResponse> = response.body() as List<DogsResponse>
+                val heroes = arrayOfNulls<String>(heroList.size)
+                for (i in heroList.indices) {
+                    heroes[i] = heroList[i].images.toString()
                 }
-                hideKeyboard()
+                val rvDog = findViewById<ImageView>(R.id.imageView)
+                Picasso.get().load(heroes[0]).into(rvDog)
+
             }
-        }
+
+            override fun onFailure(call: Call<DogsResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
+//        doAsync {
+//            val call = getRetrofit().create(APIService::class.java).getCharacterByName("$query/images").execute()
+//            val puppies = call.body() as DogsResponse
+//            uiThread {
+//                if(puppies.status == "success") {
+//                    initCharacter(puppies)
+//                }else{
+//                    showErrorDialog()
+//                }
+//                hideKeyboard()
+//            }
+//        }
     }
 
     private fun showErrorDialog() {
